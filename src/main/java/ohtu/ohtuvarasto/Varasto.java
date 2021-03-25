80a6532ffc71064822f51b1e824926378b1c01f2
AnttiHalmetoja <anttihalmetoja@gmail.com>
2021-03-25 10:41:50 +0200

    checkstyle added

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -1,6 +1,7 @@
 package ohtu.ohtuvarasto;
 
-public class Varasto {
+public class Varasto 
+{
 
     // --- piilotettu tietorakenteen toteutus: ---
     private double tilavuus;  // paljonko varastoon mahtuu,  > 0
@@ -20,18 +21,11 @@ public class Varasto {
     public Varasto(double tilavuus, double alkuSaldo) { // kuormitetaan
         if (tilavuus > 0.0) {
             this.tilavuus = tilavuus;
-        } else // virheellinen, nollataan
-        {
-            this.tilavuus = 0.0;  // => käyttökelvoton varasto
-        }
-        if (alkuSaldo < 0.0) {
-            this.saldo = 0.0;
-        } else if (alkuSaldo <= tilavuus) // mahtuu
-        {
-            this.saldo = alkuSaldo;
-        } else {
-            this.saldo = tilavuus;  // täyteen ja ylimäärä hukkaan!
-        }
+            if (tilavuus > 6) {
+                this.saldo = 3.0;
+            }
+        } 
+        
     }
 
     // --- ottavat aksessorit eli getterit: ---
